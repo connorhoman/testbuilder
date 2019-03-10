ea821c3073a12c8858e3a867fd8edb92543e029d
Connor Homan <connorhoman14@gmail.com>
2019-03-10 14:20:03 -0700

    Added Diner's Club and American Express

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,6 +8,12 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+	if (cardNumber.length === 14) {
+		return "Diner's Club";
+	}
+	if (cardNumber.length === 15) {
+		return "American Express";
+	}	
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
