dc88117f566398cc8e410bdf408155bd10290976
Connor Homan <connorhoman14@gmail.com>
2019-03-10 17:18:05 -0700

    Added Switch and Switch tests.  Refactored all other cards

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,31 +9,35 @@
 
 var detectNetwork = function(cardNumber) {
 	var length = cardNumber.length;
-	var first = cardNumber.split('')[0];
-	var second = cardNumber.split('')[1];
+	
 
-	if (length === 14 && first === '3') {
+	if (length === 14 && (cardNumber.indexOf('38') === 0 || cardNumber.indexOf('39') === 0)) {
 		return "Diner's Club";
 	}
 
-	if (length === 15 && first === '3') {
+	if (length === 15 && (cardNumber.indexOf('34') === 0 || cardNumber.indexOf('37') === 0)) {
 		return 'American Express';
 	}
 
-	if (first === '4') {
-		return 'Visa';
-	}
-
-	if (first === '5' && second !== '0') {
+	if (length === 16 && (cardNumber.indexOf('51') === 0 || cardNumber.indexOf('52') === 0 || cardNumber.indexOf('53') === 0 || cardNumber.indexOf('54') === 0 || cardNumber.indexOf('55') === 0)) {
 		return 'MasterCard';
 	}
 
-	
-	if ((first === '6' && second === '3') || (first === '5' && second === '0')) {
+	if (cardNumber.indexOf('5018') === 0 || cardNumber.indexOf('5020') === 0 || cardNumber.indexOf('5038') === 0 || cardNumber.indexOf('6304') === 0) {
 		return 'Maestro';
 	}
-	if (first === '6' && (length === 16 || length === 19)) {
+	if ((length === 16 || length === 19) && (cardNumber.indexOf('6011') === 0 || cardNumber.indexOf('65') === 0 || cardNumber.indexOf('644') === 0 || cardNumber.indexOf('645') === 0 || cardNumber.indexOf('646') === 0 || cardNumber.indexOf('647') === 0 || cardNumber.indexOf('648') === 0 || cardNumber.indexOf('649') === 0)) {
 		return 'Discover';
+	}
+
+
+	if ((length === 16 || length === 18 || length === 19) && (cardNumber.indexOf('4903') === 0 || cardNumber.indexOf('4905') === 0 || cardNumber.indexOf('4911') === 0 || cardNumber.indexOf('4936') === 0 || cardNumber.indexOf('564182') === 0 || cardNumber.indexOf('633110') === 0 || cardNumber.indexOf('6333') === 0 || cardNumber.indexOf('6759') === 0)) {
+		return 'Switch';
+	}
+	
+
+	if ((length === 13 || length === 16 || length === 19) && (cardNumber.indexOf('4') === 0)) {
+		return 'Visa';
 	}
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
@@ -41,5 +45,4 @@ var detectNetwork = function(cardNumber) {
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
 
